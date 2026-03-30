c3ec6021b2e4851bf53e69935cdb71c844218760
fatalefrequencia <fatalefrequenica@gmail.com>
2026-03-30 05:29:14 -0400

    Aesthetic: Update skull icon to match exact anatomical reference outline

--- src/components/discovery/OrganicSkull.jsx
+++ src/components/discovery/OrganicSkull.jsx
@@ -4,30 +4,26 @@ const OrganicSkull = ({ size = 24, color = 'currentColor', ...props }) => (
     <svg 
         width={size} 
         height={size} 
-        viewBox="0 0 24 24" 
+        viewBox="0 0 512 512" 
         fill="none" 
         stroke={color} 
-        strokeWidth="1.5" 
+        strokeWidth="24" 
         strokeLinecap="round" 
         strokeLinejoin="round" 
-        style={{ filter: `drop-shadow(0 0 2px ${color}66)` }}
         {...props}
     >
-        {/* Skull Main Form */}
-        <path d="M12 3c-4.5 0-8 3.5-8 8 0 3 1.5 5 2.5 7.5.5 1 0 2.5 0 3.5h11c0-1-.5-2.5 0-3.5 1-2.5 2.5-4.5 2.5-7.5 0-4.5-3.5-8-8-8z" />
+        {/* Main Skull Outline - Based on provided reference */}
+        <path d="M256,48C150.13,48,64,134.13,64,240c0,41.22,12.91,79.37,34.89,110.74L96,384l32,16,16,48h64l16-32h64l16,32h64l16-48,32-16-2.89-33.26C435.09,319.37,448,281.22,448,240,448,134.13,361.87,48,256,48Z" />
         
-        {/* Eyes - More organic socket shapes */}
-        <path d="M9 11.5c.5-1 2-1 2.5 0 .2.5 0 1.5-.5 2-1 1-2.5 0-2-2z" fill={color} fillOpacity="0.2" />
-        <path d="M15 11.5c-.5-1-2-1-2.5 0-.2.5 0 1.5.5 2 1 1 2.5 0 2-2z" fill={color} fillOpacity="0.2" />
+        {/* Eye Sockets */}
+        <path d="M192,224c-26.51,0-48,21.49-48,48s21.49,48,48,48,48-21.49,48-48S218.51,224,192,224Z" fill={color} fillOpacity="0.1" />
+        <path d="M320,224c-26.51,0-48,21.49-48,48s21.49,48,48,48,48-21.49,48-48S346.51,224,320,224Z" fill={color} fillOpacity="0.1" />
         
-        {/* Nose - Triangular void */}
-        <path d="M11.5 15.5l.5-1 .5 1h-1z" fill={color} />
+        {/* Nasal Cavity */}
+        <path d="M256,336l-16,32h32Z" fill={color} />
         
-        {/* Teeth/Jaw detail */}
-        <path d="M10 20v2M12 20v2M14 20v2" opacity="0.5" />
-
-        {/* Crossbones - Atmospheric/Subtle */}
-        <path d="M3 21l3-3M21 21l-3-3M3 3l3 3M21 3l-3 3" opacity="0.4" />
+        {/* Teeth Lines */}
+        <path d="M192,416v16M224,416v16M256,416v16M288,416v16M320,416v16" strokeWidth="12" opacity="0.6" />
     </svg>
 );
 
