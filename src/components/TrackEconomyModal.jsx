04988764f8b43f0753d6555c378fc4b2e9ab8f54
fatalefrequencia <fatalefrequencia@users.noreply.github.com>
2026-06-10 00:08:46 -0400

    tips5

--- src/components/TrackEconomyModal.jsx
+++ src/components/TrackEconomyModal.jsx
@@ -79,7 +79,7 @@ const TrackEconomyModal = ({
                         className="w-full max-w-lg bg-[#050505] border border-[#ff006e]/30 relative overflow-hidden shadow-[0_0_80px_rgba(255,0,110,0.2)] rounded-sm font-mono text-white"
                     >
                         {/* Status bar */}
-                        <div className="absolute top-0 left-0 right-0 h-1 bg-gradient-to-r from-[#ff006e] via-[#d60036] to-[#9d00ff]" />
+                        <div className="absolute top-0 left-0 right-0 h-1 bg-gradient-to-r from-[#ff006e] via-[#9d00ff] to-[#d60036]" />
 
                         {/* Top close button */}
                         <div className="flex justify-between items-center p-5 border-b border-white/5 bg-black/50">
